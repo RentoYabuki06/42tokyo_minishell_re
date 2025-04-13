1305ae8f59fe9767181c2ce728ba01e2c6d2411d
my-nozomi <mai.yokono@nozomi-ai.co.jp>
2025-04-13 13:44:03 +0900

    norm途中

--- srcs/executor/redirects.c
+++ srcs/executor/redirects.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirects.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryabuki <ryabuki@student.42.fr>            +#+  +:+       +#+        */
+/*   By: myokono <myokono@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 12:54:49 by myokono           #+#    #+#             */
-/*   Updated: 2025/04/12 21:06:21 by ryabuki          ###   ########.fr       */
+/*   Updated: 2025/04/13 13:39:17 by myokono          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,135 +61,43 @@ static int	setup_redir(t_command *cmd, t_token_type type, char *filename)
 	return (ERROR);
 }
 
-// int	setup_redirects(t_command *cmd)
-// {
-// 	t_token	*token;
-// 	t_token	*next;
-
-// 	token = cmd->redirects;
-// 	while (token)
-// 	{
-// 		next = token->next;
-// 		if (!next)
-// 		{
-// 			error_message("Missing filename for redirection");
-// 			return (ERROR);
-// 		}
-// 		if (setup_redir(cmd, token->type, next->value) == ERROR)
-// 			return (ERROR);
-// 		token = next->next;
-// 	}
-// 	return (SUCCESS);
-// }
-
-
-
-// int setup_redirects(t_command *cmd)
-// {
-// 	t_token *token;
-// 	t_token *next;
-	
-// 	// ステップ1: 入力リダイレクト（<）を処理
-// 	token = cmd->redirects;
-// 	while (token)
-// 	{
-// 		next = token->next;
-// 		if (!next)
-// 		{
-// 			error_message("Missing filename for redirection");
-// 			return (ERROR);
-// 		}
-// 		if (token->type == TOKEN_REDIRECT_IN)
-// 		{
-// 			if (setup_redir(cmd, token->type, next->value) == ERROR)
-// 				return (ERROR);
-// 		}
-// 		token = next->next;
-// 	}
-	
-// 	// ステップ2: 最後のヒアドキュメント（<<）を処理
-// 	t_token *last_heredoc = NULL;
-// 	t_token *last_heredoc_value = NULL;
-// 	token = cmd->redirects;
-// 	while (token)
-// 	{
-// 		next = token->next;
-// 		if (!next)
-// 			break;
-// 		if (token->type == TOKEN_HEREDOC)
-// 		{
-// 			last_heredoc = token;
-// 			last_heredoc_value = next;
-// 		}
-// 		token = next->next;
-// 	}
-// 	if (last_heredoc && last_heredoc_value)
-// 	{
-// 		if (setup_redir(cmd, last_heredoc->type, last_heredoc_value->value) == ERROR)
-// 			return (ERROR);
-// 	}
-
-// 	// ステップ3: 出力リダイレクト（>、>>）を処理
-// 	token = cmd->redirects;
-// 	while (token)
-// 	{
-// 		next = token->next;
-// 		if (!next)
-// 			break;
-		
-// 		if (token->type == TOKEN_REDIRECT_OUT || token->type == TOKEN_APPEND)
-// 		{
-// 			if (setup_redir(cmd, token->type, next->value) == ERROR)
-// 				return (ERROR);
-// 		}
-		
-// 		token = next->next;
-// 	}
-	
-// 	return (SUCCESS);
-// }
-
-int setup_redirects(t_command *cmd)
+int	setup_redirects(t_command *cmd)
 {
-	t_token *token;
-	t_token *next;
+	t_token	*token;
+	t_token	*next;
+	int		last_heredoc_fd;
+	int		heredoc_fd;
 
-	// 1. ヒアドキュメント（<<）を前から処理
-	int last_heredoc_fd = -1;
-
+	last_heredoc_fd = -1;
 	token = cmd->redirects;
 	while (token)
 	{
 		next = token->next;
 		if (!next)
-			break;
+			break ;
 		if (token->type == TOKEN_HEREDOC)
 		{
-			int heredoc_fd = setup_redir_return_fd(token->type, next->value);
+			heredoc_fd = setup_redir_return_fd(token->type, next->value);
 			if (heredoc_fd == -1)
 				return (ERROR);
 			if (last_heredoc_fd != -1)
-				close(last_heredoc_fd);  // 前の heredoc fd は閉じる
+				close(last_heredoc_fd);
 			last_heredoc_fd = heredoc_fd;
 		}
 		token = next->next;
 	}
-	
-	// 最後の heredoc の fd を input_fd に設定
 	if (last_heredoc_fd != -1)
 	{
 		if (cmd->input_fd != STDIN_FILENO)
 			close(cmd->input_fd);
 		cmd->input_fd = last_heredoc_fd;
 	}
-	
-
-	// 2. 出力（>、>>）
 	token = cmd->redirects;
 	while (token)
 	{
 		next = token->next;
-		if (!next) break;
+		if (!next)
+			break ;
 		if (token->type == TOKEN_REDIRECT_OUT || token->type == TOKEN_APPEND)
 		{
 			if (setup_redir(cmd, token->type, next->value) == ERROR)
@@ -197,8 +105,6 @@ int setup_redirects(t_command *cmd)
 		}
 		token = next->next;
 	}
-
-	// 3. 入力（<）※HEREDOC より後に処理されると上書きしてしまうので注意
 	token = cmd->redirects;
 	while (token)
 	{
@@ -215,6 +121,5 @@ int setup_redirects(t_command *cmd)
 		}
 		token = next->next;
 	}
-
 	return (SUCCESS);
 }
